7ae1898a9479d98ecefe4045853aff6e0a04d871
Pedro Fellipe Cruz Antunes <129354494+PedroFellipeAntunes@users.noreply.github.com>
2024-10-02 03:17:03 -0300

    Add files via upload

--- src/main/java/Windows/SnowflakeWindow.java
+++ src/main/java/Windows/SnowflakeWindow.java
@@ -1,8 +1,4 @@
 /*
-TODO: Implement raycasting based on angle using spatial grouping
-*/
-
-/*
 Koch Snowflake
 Based on The Coding Train:
     Coding Challenge #129: Koch Fractal Snowflake - https://www.youtube.com/watch?v=X8bXDKqMsXE
@@ -30,10 +26,10 @@ public class SnowflakeWindow {
     private JTextField vertTextField;
     
     //Light control
-    private final int reflectionLimit = 10;
-    private double castAngle = 0.0;
+    private final int reflectionLimit = 15;
+    private double castAngle = 25.0;
     
-    private ArrayList<Line> raycast = new ArrayList<>();
+    private final ArrayList<Line> raycast = new ArrayList<>();
     
     //Fractal control
     private final int polygonRadius = 250;
@@ -42,7 +38,6 @@ public class SnowflakeWindow {
     private int generation = 0;
     
     private ArrayList<Line> snowflake = new ArrayList<>();
-    private ArrayList<ArrayList<Line>> recursivePolygons = new ArrayList<>();
     
     private double zoom = 1.0;
     private double rotationAngle = 0.0;
@@ -59,8 +54,7 @@ public class SnowflakeWindow {
             createFirstLine();
         } else {
             snowflake = createPolygon(windowW / 2, windowH / 2, polygonRadius, vertices);
-//            createRecursivePolygons(windowW / 2, windowH / 2, polygonRadius, vertices);
-//            raycasting();
+            raycasting();
         }
         
         //Generation counting label
@@ -78,6 +72,7 @@ public class SnowflakeWindow {
         controlLabel = new JLabel("<html>ENTER == Next generation<br>"
                 + "BACKSPACE || DELETE == reset<br>"
                 + "RIGHT || LEFT == rotate<br>"
+                + "UP || DOWN == rotate reflection<br>"
                 + "+ || - == ZOOM</html>");
         controlLabel.setBackground(Color.BLACK);
         controlLabel.setForeground(Color.WHITE);
@@ -161,6 +156,8 @@ public class SnowflakeWindow {
                     case KeyEvent.VK_MINUS -> zoomOut();
                     case KeyEvent.VK_LEFT -> rotateLeft();
                     case KeyEvent.VK_RIGHT -> rotateRight();
+                    case KeyEvent.VK_UP -> angleUp();
+                    case KeyEvent.VK_DOWN -> angleDown();
                     case KeyEvent.VK_ENTER -> nextGeneration();
                     case KeyEvent.VK_DELETE, KeyEvent.VK_BACK_SPACE -> reset();
                 }
@@ -178,13 +175,12 @@ public class SnowflakeWindow {
         }
         
         snowflake = tempSnowflake;
-//        recursivePolygons.set(recursivePolygons.size() - 1, snowflake);
+        raycasting();
         generation++;
         genLabel.setText("" + generation);
     }
     
     private void reset() {
-        recursivePolygons.clear();
         snowflake.clear();
         raycast.clear();
         
@@ -192,8 +188,7 @@ public class SnowflakeWindow {
             createFirstLine();
         } else {
             snowflake = createPolygon(windowW / 2, windowH / 2, polygonRadius, vertices);
-//            createRecursivePolygons(windowW / 2, windowH / 2, polygonRadius, vertices);
-//            raycasting();
+            raycasting();
         }
         
         generation = 0;
@@ -243,38 +238,9 @@ public class SnowflakeWindow {
         return newPolygon;
     }
     
-    private void createRecursivePolygons(int centralX, int centralY, int radius, int verticeCount) {
-        //Create snowflake first
-        snowflake = createPolygon(centralX, centralY, radius, verticeCount);
-        
-        if (verticeCount <= 3) {
-            return;
-        }
-        
-        int currentVerticeCount = lowestDivisible(verticeCount - 1);
-        System.out.println("");
-        //Divide by 3 until reaches triangle
-        while (currentVerticeCount >= 3) {
-            System.out.print(currentVerticeCount+", ");
-            ArrayList<Line> newPolygon = createPolygon(centralX, centralY, radius, currentVerticeCount);
-            recursivePolygons.add(newPolygon);
-            
-            currentVerticeCount /= 3;
-        }
-        
-        //Add to end of list
-        recursivePolygons.add(snowflake);
-    }
-    
-    private int lowestDivisible(int verticeCount) {
-        while (verticeCount % 3 != 0) {
-            verticeCount--;
-        }
-        
-        return verticeCount;
-    }
-    
     private void raycasting() {
+        raycast.clear();
+        
         // Define hard start point for first line
         int centerX = windowW / 2;
         int centerY = windowH / 2;
@@ -286,18 +252,16 @@ public class SnowflakeWindow {
         double angleInRadians = Math.toRadians(castAngle);
         
         startingLine.setP2(genericEndPoint(centerX, centerY, angleInRadians));
-        System.out.println("CHECKING FOR FIRST LINE");
-        Point collisionPoint = recursiveCollisionSearch(startingLine);
+        
+        Line collisionLine = findClosestCollision(startingLine, null);
+        
+        raycast.add(startingLine);
         
         // In the unlikely case where there is no collision
-        if (collisionPoint == null) {
-            raycast.add(startingLine);
-        } else {
-            startingLine.setP2(collisionPoint);
+        if (collisionLine != null) {
+            startingLine.setP2(collisionLine.getCollisionPoint());
             
-            raycast.add(startingLine);
-            
-            processReflections(raycast.get(0), 1);
+            processReflections(collisionLine);
         }
     }
     
@@ -309,85 +273,87 @@ public class SnowflakeWindow {
         return new Point(endX, endY);
     }
     
-    private void processReflections(Line parentLine, int currentCount) {
-        System.out.println("Current COUNT: " + currentCount);
-        if (currentCount >= reflectionLimit) {
-            return;
-        }
-
-        // Calcular o ângulo da linha pai (linha de incidência)
-        double angleToLine = Math.atan2(parentLine.getP2().getY() - parentLine.getP1().getY(),
-                parentLine.getP2().getX() - parentLine.getP1().getX());
-
-        // AQUI É O PONTO ONDE A LINHA ATINGE A SUPERFÍCIE (COLISÃO)
-        Point collisionPoint = recursiveCollisionSearch(parentLine);
-        if (collisionPoint == null) {
-            return; // Não houve colisão, não faz reflexão
-        }
-
-        // Calcular a normal (supondo que o objeto é vertical, a normal é horizontal)
-        // Aqui você deve ajustar a normal com base na linha da colisão real
-        double normalAngle = angleToLine + Math.PI / 2; // Normal é perpendicular à linha
-        double angleOfIncidence = angleToLine - normalAngle; // Ângulo de incidência
-
-        // Calcular o ângulo de reflexão
-        double reflectedAngle = normalAngle - angleOfIncidence; // Reflexão é igual ao ângulo de incidência
-
-        // Criar nova linha refletida
-        Point newP1 = parentLine.getP2(); // O ponto de partida é onde a linha pai terminou
-
-        Line newLine = new Line();
-        newLine.setP1(newP1);
-        newLine.setP2(genericEndPoint(newP1.getX(), newP1.getY(), reflectedAngle));
-
-        // Verificar colisão com a nova linha refletida
-        System.out.println("CHECKING FOR REFLECTIONS");
-        Point collisionPointReflected = recursiveCollisionSearch(newLine);
-
-        // Caso não haja colisão
-        if (collisionPointReflected == null) {
+    private void processReflections(Line parentCollisionLine) {
+        Line collisionLine = parentCollisionLine;
+        
+        for (int i = 0; i < reflectionLimit; i++) {
+            Line newLine = calculateReflection(raycast.get(i), collisionLine);
+            
+            collisionLine = findClosestCollision(newLine, collisionLine);
+            
+            if (collisionLine == null) {
+                break;
+            }
+            
+            newLine.setP2(collisionLine.getCollisionPoint());
             raycast.add(newLine);
-        } else {
-            newLine.setP2(collisionPointReflected);
-            raycast.add(newLine);
-
-            // Chamada recursiva para processar a próxima reflexão
-            processReflections(newLine, currentCount + 1);
         }
     }
     
-    private Point recursiveCollisionSearch(Line rayLine) {
-        int currentIndex = 0;
+    private Line calculateReflection(Line ray, Line collisionLine) {
+        Point collisionPoint = collisionLine.getCollisionPoint();
+
+        double vx = ray.getP2().getX() - ray.getP1().getX();
+        double vy = ray.getP2().getY() - ray.getP1().getY();
+
+        double nx = collisionLine.getNx();
+        double ny = collisionLine.getNy();
+
+        double dotProduct = vx * nx + vy * ny;
         
-        ArrayList<Line> currentPolygon;
+        double reflectedVx = vx - 2 * dotProduct * nx;
+        double reflectedVy = vy - 2 * dotProduct * ny;
         
-        Point collisionPoint = null;
+        double reflectedAngle = Math.atan2(reflectedVy, reflectedVx);
         
-        for (int i = 0; i < recursivePolygons.size(); i++) {
-            currentPolygon = recursivePolygons.get(i);
-            
-            int numLines = currentPolygon.size();
-            int step = numLines / 3;
-            
-            for (int j = currentIndex; j < currentPolygon.size(); j++) {
-                Line currentLine = currentPolygon.get(j);
-                
-                collisionPoint = checkCollision(rayLine, currentLine);
-                
-                if (collisionPoint != null) {
-                    System.out.println("FOUND COLLISION ON P: "+i+", LINE: "+j);
-                    currentIndex = j;
-                    break;
-                } else {
-                    System.out.println("DOES NOT COLLIDE WITH P: "+i+", LINE: "+j);
+        Point reflectionEnd = genericEndPoint(collisionPoint.getX(), collisionPoint.getY(), reflectedAngle);
+
+        return new Line(collisionPoint, reflectionEnd);
+    }
+    
+    private Line findClosestCollision(Line ray, Line currentWall) {
+        Line closestCollision = null;
+        double closestDistance = Double.MAX_VALUE;
+
+        Point p1 = ray.getP1();
+
+        for (Line wall : snowflake) {
+            if (wall == null) {
+                continue;
+            }
+
+            //Check to see if closest line is not current collided line
+            if (currentWall != null) {
+                if (wall.compareLines(currentWall)) {
+                    continue;
+                }
+            }
+
+            Point collision = checkCollision(ray, wall);
+
+            if (collision != null) {
+                double distance = distanceBetween(p1, collision);
+
+                if (distance < closestDistance) {
+                    closestDistance = distance;
+
+                    closestCollision = new Line(wall);
+                    closestCollision.setCollisionPoint(collision);
                 }
             }
         }
         
-        return collisionPoint;
+        return closestCollision;
     }
     
-    public static Point checkCollision(Line lineA, Line lineB) {
+    private double distanceBetween(Point p1, Point p2) {
+        double dx = p1.getX() - p2.getX();
+        double dy = p1.getY() - p2.getY();
+        
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+    
+    private Point checkCollision(Line lineA, Line lineB) {
         double x1 = lineA.getP1().getX();
         double y1 = lineA.getP1().getY();
         double x2 = lineA.getP2().getX();
@@ -406,19 +372,17 @@ public class SnowflakeWindow {
         double intersectX = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / denominator;
         double intersectY = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / denominator;
 
-        //return new Point(intersectX, intersectY);
-        
-        // Verifica se o ponto de interseção está dentro dos limites dos segmentos de linha
-        if (isBetween(x1, x2, intersectX) && isBetween(y1, y2, intersectY) &&
-            isBetween(x3, x4, intersectX) && isBetween(y3, y4, intersectY)) {
+        if (isBetween(x1, x2, intersectX) && isBetween(y1, y2, intersectY)
+                && isBetween(x3, x4, intersectX) && isBetween(y3, y4, intersectY)) {
             return new Point(intersectX, intersectY);
-        } else {
-            return null; // O ponto de interseção não está dentro dos segmentos de linha
         }
+
+        return null;
     }
-    
-    private static boolean isBetween(double a, double b, double c) {
-        return c >= Math.min(a, b) && c <= Math.max(a, b);
+
+    private boolean isBetween(double a, double b, double c) {
+        double tolerance = 1e-9;
+        return (c >= Math.min(a, b) - tolerance && c <= Math.max(a, b) + tolerance);
     }
     
     private ArrayList<Line> breakLine(Line line) {
@@ -503,6 +467,18 @@ public class SnowflakeWindow {
         rotationAngle += Math.PI / 180.0;
     }
     
+    public void angleUp() {
+        castAngle += 12 * (Math.PI / 180.0);
+        raycasting();
+        panel.repaint();
+    }
+    
+    public void angleDown() {
+        castAngle -= 12 * (Math.PI / 180.0);
+        raycasting();
+        panel.repaint();
+    }
+    
     private class DrawPanel extends JPanel {
         @Override
         protected void paintComponent(Graphics g) {
@@ -512,24 +488,28 @@ public class SnowflakeWindow {
             g2d.scale(zoom, zoom);
             g2d.rotate(rotationAngle, panel.getWidth() / 2, panel.getHeight() / 2);
             
+            //Draw raycast
+            // Desenhar raycast
+            if (polygon) {
+                for (int i = raycast.size() - 1; i >= 0; i--) {
+                    float bri = (float) (raycast.size() - i) / raycast.size();
+
+                    g.setColor(Color.getHSBColor(1.0f, 1.0f, bri));
+
+                    Line current = raycast.get(i);
+
+                    g.drawLine((int) current.getP1().getX(),
+                            (int) current.getP1().getY(),
+                            (int) current.getP2().getX(),
+                            (int) current.getP2().getY());
+
+//                    g.drawOval((int) current.getP2().getX() - 5,
+//                            (int) current.getP2().getY() - 5, 10, 10);
+                }
+            }
+            
             //Draw snowflake
             g.setColor(Color.WHITE);
-            
-//            for (int j = 0; j < recursivePolygons.size(); j++) {
-//                ArrayList<Line> polygon = recursivePolygons.get(j);
-//                
-//                float hue = (float) j / recursivePolygons.size(); // Normaliza o hue entre 0 e 1
-//                g.setColor(Color.getHSBColor(hue, 1.0f, 1.0f));
-//                
-//                for (int i = 0; i < polygon.size(); i++) {
-//                    Line current = polygon.get(i);
-//
-//                    g.drawLine((int) current.getP1().getX(),
-//                            (int) current.getP1().getY(),
-//                            (int) current.getP2().getX(),
-//                            (int) current.getP2().getY());
-//                }
-//            }
             
             for (int i = 0; i < snowflake.size(); i++) {
                 Line current = snowflake.get(i);
@@ -539,23 +519,6 @@ public class SnowflakeWindow {
                         (int) current.getP2().getX(),
                         (int) current.getP2().getY());
             }
-            
-            //Draw raycast
-//            if (polygon) {
-//                g.setColor(Color.WHITE);
-//
-//                for (int i = 0; i < raycast.size(); i++) {
-//                    Line current = raycast.get(i);
-//                    
-//                    g.drawLine((int) current.getP1().getX(),
-//                            (int) current.getP1().getY(),
-//                            (int) current.getP2().getX(),
-//                            (int) current.getP2().getY());
-//                    
-//                    g.drawOval((int) current.getP2().getX() - 5,
-//                            (int) current.getP2().getY() - 5, 10, 10);
-//                }
-//            }
         }
     }
 }
